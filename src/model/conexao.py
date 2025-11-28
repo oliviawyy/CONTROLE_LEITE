8ac4102967abca771d6c95a28bd86e58b5507b52
oliviawyy <olivia.c@aluno.senai.br>
2025-11-28 14:25:11 -0300

    hmm

---
+++ src/model/conexao.py
@@ -0,0 +1,18 @@
+import mysql.connector
+
+def criar_conexao():
+    conexao = mysql.connector,connect(
+                                    host="localhost",
+                                    port=3306,
+                                    user="root",
+                                    password="root",
+                                    database="bd.controlleite"
+                                )
+
+    cursor = conexao.cursor()
+
+    return conexao, cursor
+
+if __name__ == "__main__":
+   conexao, cursor = criar_conexao()
+   print(conexao)
